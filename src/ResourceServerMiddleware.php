5ef44c3b207332578f1bf02dc2cadc89316d28cd
phprestfulapi <125196752+phprestfulapi@users.noreply.github.com>
2023-02-12 19:11:43 +0100

    feat: Implement ResourceServerMiddleware

---
+++ src/ResourceServerMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpRestfulapi\ResourceServerMiddleware;
+
+use Exception;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\ResourceServer;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
+
+class ResourceServerMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly ResourceServer $server,
+        private readonly ResponseFactoryInterface $responseFactory
+    ) {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            $request = $this->server->validateAuthenticatedRequest($request);
+        } catch (OAuthServerException $exception) {
+            return $exception->generateHttpResponse($this->responseFactory->createResponse());
+        } catch (Throwable $exception) {
+            return (new OAuthServerException(
+                $exception->getMessage(),
+                0,
+                'unknown_error',
+                500
+            ))->generateHttpResponse($this->responseFactory->createResponse());
+        }
+
+        return $handler->handle($request);
+    }
+}
